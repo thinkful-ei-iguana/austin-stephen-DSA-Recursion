3c3d7de281f5781086b44250a023c4926921d934
AustinT33 <austintumlinson@gmail.com>
2020-01-21 11:39:17 -0600

    solved powerCalc

--- DNA-Recursion.js
+++ DNA-Recursion.js
@@ -1,10 +1,26 @@
-const countingSheep = function(num) {
-  if (num === 0) {
-    console.log('All sheep jumped over the fence');
-  } else {
-    console.log(`${num}: Another sheep jumps over the fence`);
-    countingSheep(num - 1);
-  }
-};
+// const countingSheep = function(num) {
+//   if (num === 0) {
+//     console.log('All sheep jumped over the fence');
+//   } else {
+//     console.log(`${num}: Another sheep jumps over the fence`);
+//     countingSheep(num - 1);
+//   }
+// };
 
-countingSheep(3);
+// countingSheep(3);
+
+const powerCalc = function(num, exp) {
+    
+    if (exp <= 0) {
+        console.log('exponent should be >= 0')
+    } else if (exp === 1) {
+         return num 
+    } else {
+        return num * powerCalc(num, exp -1)
+        }     
+    };
+    const test = powerCalc(2, 4);
+    console.log(test);
+
+    
+   
