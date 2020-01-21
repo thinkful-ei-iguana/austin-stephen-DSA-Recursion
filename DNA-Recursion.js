8b378a3043196719c6d9dea9f0e34b1c0aca4c7d
stephenschab <sschab689@gmail.com>
2020-01-21 12:27:11 -0700

    added reverse string function

--- DNA-Recursion.js
+++ DNA-Recursion.js
@@ -9,18 +9,29 @@
 
 // countingSheep(3);
 
-const powerCalc = function(num, exp) {
+// const powerCalc = function(num, exp) {
     
-    if (exp <= 0) {
-        console.log('exponent should be >= 0')
-    } else if (exp === 1) {
-         return num 
-    } else {
-        return num * powerCalc(num, exp -1)
-        }     
-    };
-    const test = powerCalc(2, 4);
-    console.log(test);
+//     if (exp <= 0) {
+//         console.log('exponent should be >= 0')
+//     } else if (exp === 1) {
+//          return num 
+//     } else {
+//         return num * powerCalc(num, exp -1)
+//         }     
+//     };
+//     const test = powerCalc(2, 4);
+//     console.log(test);
+
+const reverseString = function(str) {
+  if(str.length === 0) {
+    return '';
+  } else {
+    return str[str.length - 1] + reverseString(str.substring(0, str.length -1));
+  }
+};
+
+let test = 'cat';
+console.log(reverseString(test));
 
     
    
